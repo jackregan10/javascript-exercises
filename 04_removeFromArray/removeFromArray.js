25464e85ccdac00b1b43629fbefa7206d1734b1e
Jack Regan <108939649+jackregan10@users.noreply.github.com>
2024-05-27 10:20:13 -0400

    Finish the first seven exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,27 @@
 const removeFromArray = function() {
+    for (let j = 1; j < arguments.length; j++) {
+        for (let i = 0; i < arguments[0].length; i++) {
+            if (arguments[0][i] === arguments[j]) {
+                arguments[0].splice(i, 1);
+                i--;
+            }
+        }
+    }
+    return arguments[0];
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+//Alternate solution with better runtime but worse space complexity
+/*
+    const newArray = [];
+    array.forEach((item) => {
+      if (!args.includes(item)) {
+        newArray.push(item);
+      }
+    });
+    return newArray;
+  };
+  */
